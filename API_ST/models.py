074204eb80cd280f194fb04dcc60ba6ca2c4b73f
Rayssa Chaves <rayssa.ccmelo@gmail.com>
2025-08-28 13:19:48 -0300

    Update models.py

--- API_ST/models.py
+++ API_ST/models.py
@@ -2,7 +2,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 class StrangerThingsCharacters(BaseModel):
-    
     id: Optional[int] = None
     nome: str
     status: str
@@ -10,3 +9,12 @@ class StrangerThingsCharacters(BaseModel):
     habilidades: str
     familia: str
     foto: str
+
+# Modelo para PATCH (edição parcial do personagem)
+class StrangerThingsCharactersPatch(BaseModel):
+    nome: Optional[str] = None
+    status: Optional[str] = None
+    ocupaçao: Optional[str] = None
+    habilidades: Optional[str] = None
+    familia: Optional[str] = None
+    foto: Optional[str] = None
