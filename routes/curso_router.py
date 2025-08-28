7d31d851b87210558c8f90fc606bfa0a766bb794
RAYsSA-Chaves <rayssa.ccmelo@gmail.com>
2025-08-28 19:27:50 -0300

    arrumando as pastas

--- routes/curso_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/api/v1/cursos")
-async def get_cursos():
-    return {"info: Todos os cursos"}
--- routes/usuario_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get("/api/v1/usuarios")
-async def get_users():
-    return {"info: Todos os usuários"}
